165587528798abd0d0c5d7cef5dd8816525c2d4b
mqm1999 <61899832+mqm1999@users.noreply.github.com>
2020-08-10 01:34:35 +0700

    Update TeacherManagement.java

--- JPL.S.L201/src/fa/training/management/TeacherManagement.java
+++ JPL.S.L201/src/fa/training/management/TeacherManagement.java
@@ -24,22 +24,22 @@ public class TeacherManagement {
 			System.out.println("College name: " + teacher[i].getCollegename());
 			System.out.println("Disgnation: " + teacher[i].getDesignation());
 			if (teacher[i] instanceof MathTeacher) {
-				System.out.println("Main subject: " + ((MathTeacher) teacher[i]).getMainSubject());
+				System.out.println("Main subject: " + ((MathTeacher) teacher[i]).getMainSubject()); // in ra main subject của MathTeacher thứ i
 				
-				((MathTeacher) teacher[i]).toSchool();
+				((MathTeacher) teacher[i]).toSchool();// in ra dòng "Math teacher go to school by car!
 				
 				teacher[i].teach();
 				System.out.println("SUM(" + number1 + ", " + number2 + ") = " + 
 				mathTeacher.sum(number1, number2));
 				
 			} else {
-				System.out.println("Main subject: " + ((EnglishTeacher) teacher[i]).getMainSubject());
+				System.out.println("Main subject: " + ((EnglishTeacher) teacher[i]).getMainSubject());// in ra main subject của EnglishTeacher thứ i
 				
-				((EnglishTeacher) teacher[i]).toSchool();
+				((EnglishTeacher) teacher[i]).toSchool();// in ra dòng "English Teacher go to school by motorbike
 				
 				teacher[i].teach();
 				
-				((EnglishTeacher) teacher[i]).translate("Hello", "Xin chao!");
+				((EnglishTeacher) teacher[i]).translate("Hello", "Xin chao!");// in ra dòng "Hello in Vietnamese Xin chao!"
 			}
 			
 		}
